cd840c253e208ee947be51f676142c16cf079dbe
tapso96 <das.rohit897@gmail.com>
2020-04-20 18:58:56 +0530

    All files are Upadeted

---
+++ signup.php
@@ -0,0 +1,36 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Signup</title>
+	
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css3/style3.css">
+</head>
+<body>
+	<form action="signup.php" method="POST" >
+	<div class="row">
+		<a href="index.html"><img src="F:\website\images\logo.jpeg"  class="logo"></a>
+	</div>			
+	<div class="signup">
+			<h1>Signup</h1>
+		<div class="textbox">	
+			<input type="text" placeholder="Name" name="username" value="">
+		</div>
+		<div class="textbox">
+			<input type="text" placeholder="E-mail" name="email" value="">
+		</div>
+		
+		<div class="textbox">
+			<input type="text" placeholder="Mobile-no" name="mobile" value="">
+		</div>
+		<div class="textbox">
+			<input type="password" placeholder="Password" name="password" value="">
+		</div>
+
+		<input type="submit" name="submit" value="submit"/>
+	
+	</form>
+</body>
+</html>
